63261dea35cc8f757776c4ab0cc95acebe1403b0
nadaJab <nadasenkez@gmail.com>
2018-12-11 10:27:49 +0100

    add script

--- Documentation/script-sql/script-insertion.sql
+++
@@ -1,6 +0,0 @@
--- table account --
-INSERT INTO account (email, password) VALUES ('nadasenk@gmail.fr', '$2a$10$ziiw9H3NxuTAq25lHomqceSMBd7w2OMl.yXZgFib9MqB2TS8gwTY6');
-
--- table user_web --
-INSERT INTO user_Web (pseudo, first_name, last_name, climbing_type, birth_year, genre, role, id_compte)
-VALUES ( 'nada123', 'nada', 'senk', 'Bloc', :birthYear, '1989-09-02', 'femme', 'utilisateur', '1');
