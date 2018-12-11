d81b4875a2fb7436d61ed39f2b658fc8ff2e64e2
nadaJab <nadasenkez@gmail.com>
2018-12-11 13:38:00 +0100

    modifier script user_0.0.1.sql

--- Documentation/script-sql/user_0.0.1.sql
+++ Documentation/script-sql/user_0.0.1.sql
@@ -1,2 +1,2 @@
 INSERT INTO user_Web (pseudo, first_name, last_name, climbing_type, birth_year, genre, role, id_compte)
-VALUES ( 'nada123', 'nada', 'senk', 'Bloc', :birthYear, '1989-09-02', 'femme', 'utilisateur', 1);
+VALUES ( 'nada123', 'nada', 'senk', 'Bloc', '1989-09-02', 'femme', 'utilisateur', 1);
